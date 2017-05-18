8f1fac46591289062419a580dbef3689e7e3dc52
Adrien <admanaus@gmail.com>
2017-05-18 14:25:40 -0600

    added checking withdraw  - refined app.js for testing

--- app.ts
+++ app.ts
@@ -7,18 +7,22 @@ import {Person} from "./classes/person";
 
 let adrienBDay: Date = new Date("1983/7/8");
 let adrien: Person = new Person("Adrien", adrienBDay );
+adrien.checkingAccount = new Account(adrien, AccountType.checking);
+adrien.savingsAccount = new Account(adrien, AccountType.savings);
+adrien.retirementAccount = new Account(adrien, AccountType.retirement);
 
-let adrienAccount = new Account(adrien, AccountType.retirement);
+adrien.checkingAccount.advanceDate(100);
+adrien.savingsAccount.advanceDate(200);
+adrien.retirementAccount.advanceDate(300);
 
-console.log(adrienAccount);
-console.log(AccountType.checking);
-console.log(AccountType.retirement);
+adrien.checkingAccount.depositMoney(100, "Paycheck");
+adrien.checkingAccount.depositMoney(-10, "Test");
+console.log(adrien.checkingAccount.accountHistory);
 
-adrienAccount.advanceDate(30);
-adrienAccount.advanceDate(365);
-adrienAccount.advanceDate(1000);
-adrienAccount.advanceDate(5);
-adrienAccount.depositMoney(100, "payCheck");
-adrienAccount.depositMoney(0, "eewps");
-adrienAccount.depositMoney(-10, 'try that!');
-console.log(adrienAccount.accountHistory);
+adrien.savingsAccount.depositMoney(100, "Savings Check");
+adrien.savingsAccount.depositMoney(-10, "Savings Test");
+console.log(adrien.savingsAccount.accountHistory);
+
+adrien.retirementAccount.depositMoney(100, "Retirement Check");
+adrien.retirementAccount.depositMoney(-10, "Retirement Test");
+console.log(adrien.retirementAccount.accountHistory);
--- classes/account.ts
+++ classes/account.ts
@@ -8,6 +8,7 @@ import * as moment from "moment";
 class Account implements IAccount {
     accountHolderName: string;
     accountHolderBirthDate: Date;
+    accountCreationDate: Date = new Date();
     currentDate: Date = new Date();
     accountAgeMonths: number = 0;
     balance: number;
@@ -34,29 +35,36 @@ class Account implements IAccount {
     withdrawMoney(amount: number,
                   description: string,
                   transactionOrigin: TransactionOrigin) : Transaction {
-        //function code here
+
+        let transaction = new Transaction;
+        transaction.amount = amount;
+        transaction.description = description;
+
+        if (this.accountType == AccountType.checking){
+            let completedTransaction: Transaction = this._checkingWithdraw(transaction, transactionOrigin);
+            this.accountHistory.push(completedTransaction);
+            return completedTransaction;
+        } else if (this.accountType == AccountType.savings){
+            let completedTransaction: Transaction = this._savingsWithdraw(transaction, transactionOrigin);
+            this.accountHistory.push(completedTransaction);
+            return completedTransaction;
+        } else if (this.accountType == AccountType.retirement){
+            let completedTransaction: Transaction = this._retirementWithdraw(transaction, transactionOrigin);
+            this.accountHistory.push(completedTransaction);
+            return completedTransaction;
+        }
     };
 
     depositMoney(amount: number, description: string) : Transaction {
         if (amount > 0){
-            this.balance = this.balance + amount;
-            let transaction: Transaction = new Transaction;
-            transaction.success = true;
-            transaction.amount = amount;
-            transaction.resultBalance = this.balance;
-            transaction.transactionDate = this.currentDate;
-            transaction.description = description;
-            transaction.errorMessage = "";
+            let transaction: Transaction = this._transactionSucess(amount, description);
             this.accountHistory.push(transaction);
+            return transaction;
         } else if (amount <= 0) {
-            let transaction: Transaction = new Transaction;
-            transaction.success = false;
-            transaction.amount = amount;
-            transaction.resultBalance = this.balance;
-            transaction.transactionDate = this.currentDate;
-            transaction.description = description;
-            transaction.errorMessage = "Deposit amount was 0 or less.  Deposit failed.";
+            let errorMessage: string = "Deposit amount was 0 or less.  Deposit failed.";
+            let transaction: Transaction = this._transactionFailure(amount, description, errorMessage);
             this.accountHistory.push(transaction);
+            return transaction;
         }
 
     };
@@ -73,8 +81,8 @@ class Account implements IAccount {
 
             months = months + ( years * 12);
             years = months / 12;
-            console.log(`Calender Months: ${months}`);
-            console.log(`Years: ${years}`);
+            console.log(`Calender Months Advanced: ${months}`);
+
 
             let P = this.balance;
             let r = this.interestRate;
@@ -82,10 +90,58 @@ class Account implements IAccount {
             let t = years;
 
             this.balance = P * Math.pow(( 1 + (r / n)),(n * t));
-            console.log(`Date: ${this.currentDate}`);
-            console.log(`Balance: ${this.balance}`);
+
         }
     };
+    private _transactionSucess(amount: number, description: string): Transaction{
+        this.balance = this.balance + amount;
+        let transaction: Transaction = new Transaction;
+        transaction.success = true;
+        transaction.amount = amount;
+        transaction.resultBalance = this.balance;
+        transaction.transactionDate = this.currentDate;
+        transaction.description = description;
+        transaction.errorMessage = "";
+        return transaction;
+    }
+    private _transactionFailure(amount: number,
+                                description: string,
+                                errorMessage: string ) : Transaction {
+        let transaction: Transaction = new Transaction;
+        transaction.success = false;
+        transaction.amount = amount;
+        transaction.resultBalance = this.balance;
+        transaction.transactionDate = this.currentDate;
+        transaction.description = description;
+        transaction.errorMessage = errorMessage;
+        return transaction;
+    }
+    private _checkingWithdraw(transaction: Transaction,
+                              transactionOrigin: TransactionOrigin): Transaction{
+        transaction.amount = Math.abs(transaction.amount) * (-1);
+
+        if (Math.abs(transaction.amount) <= this.balance){
+            let completedTransaction: Transaction = this._transactionSucess(
+                transaction.amount,
+                "Checking withdraw Success.");
+            return completedTransaction;
+        } else {
+            let completedTransaction: Transaction = this._transactionFailure(
+                transaction.amount,
+                "Checking withdraw failure",
+                "Insufficient funds");
+            return completedTransaction;
+        }
+
+    }
+    private _savingsWithdraw(transaction: Transaction,
+                             transactionOrigin: TransactionOrigin): Transaction{
+        return transaction;
+    }
+    private _retirementWithdraw(transaction: Transaction,
+                                transactionOrigin: TransactionOrigin): Transaction{
+        return transaction;
+    }
 
 }
 
